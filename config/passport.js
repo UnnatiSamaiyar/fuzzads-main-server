84ea5791b7a799be5e0d8e6907cebae44b21bb1d
Unnati Samaiyar <104674490+UnnatiSamaiyar@users.noreply.github.com>
2024-11-02 00:44:54 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('./models/User'); // Adjust the path as necessary
+const User = require('../models/User'); // Make sure the path is correct
+const bcrypt = require('bcrypt'); // To hash a random password
 require('dotenv').config(); // Load environment variables
 
 passport.use(new GoogleStrategy({
@@ -16,7 +17,19 @@ async (accessToken, refreshToken, profile, done) => {
       return done(null, existingUser);
     }
 
-    const newUser = new User({ googleId: profile.id });
+    // Generate a random password for Google signups
+    const randomPassword = Math.random().toString(36).slice(-8);
+    const hashedPassword = await bcrypt.hash(randomPassword, 10);
+
+    // Create a new user with Google profile data
+    const newUser = new User({
+      googleId: profile.id,
+      firstName: profile.name.givenName,
+      lastName: profile.name.familyName,
+      email: profile.emails[0].value,
+      password: hashedPassword, // Store the hashed random password
+    });
+
     await newUser.save();
     done(null, newUser);
   } catch (err) {
