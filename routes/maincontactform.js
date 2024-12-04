338bac304414c7855765f1e61b796758491b1004
Unnati Samaiyar <104674490+UnnatiSamaiyar@users.noreply.github.com>
2024-12-04 16:38:57 +0530

    Update maincontactform.js

--- routes/maincontactform.js
+++ routes/maincontactform.js
@@ -19,7 +19,7 @@ router.post('/contact', async (req, res) => {
     // Email content
     let mailOptions = {
       from: process.env.EMAIL_USER,
-      to: process.env.EMAIL_USER, // Admin email to receive the form data
+      to: 'contact@fuzzads.com', // Admin email to receive the form data
       subject: 'New Contact Form Submission',
       html: `
         <h3>Contact Form Details</h3>
@@ -37,10 +37,7 @@ router.post('/contact', async (req, res) => {
     // Send mail with defined transport object
     await transporter.sendMail(mailOptions);
 
-    res.status(200).json({ message: 'Contact form submitted successfully!' });
   } catch (error) {
-    console.error('Error sending email:', error);
-    res.status(500).json({ message: 'Something went wrong. Please try again later.' });
   }
 });
 
